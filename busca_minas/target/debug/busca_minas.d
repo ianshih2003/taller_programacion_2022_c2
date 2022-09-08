a0ddc38adb19a6a899e5bbee24f87031667e94f4
Ian Shih <ianshih2003@gmail.com>
2022-09-08 20:14:49 -0300

    Actualizo gitignore

--- busca_minas/target/debug/busca_minas.d
+++ busca_minas/target/debug/busca_minas.d
@@ -1,1 +1,1 @@
-D:\iansh\uba\taller_programacion\busca_minas\target\debug\busca_minas.exe: D:\iansh\uba\taller_programacion\busca_minas\src\main.rs
+D:\iansh\uba\taller_programacion\busca_minas\target\debug\busca_minas.exe: D:\iansh\uba\taller_programacion\busca_minas\src\lib.rs D:\iansh\uba\taller_programacion\busca_minas\src\main.rs
