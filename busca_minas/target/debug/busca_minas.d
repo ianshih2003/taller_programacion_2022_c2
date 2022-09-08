4bf7796b31df98e3c09a4a491219916a8cbdcc03
Ian Shih <ianshih2003@gmail.com>
2022-09-08 20:12:12 -0300

    Mover buscaminas a otra carpeta

---
+++ busca_minas/target/debug/busca_minas.d
@@ -0,0 +1,1 @@
+D:\iansh\uba\taller_programacion\busca_minas\target\debug\busca_minas.exe: D:\iansh\uba\taller_programacion\busca_minas\src\main.rs
---
+++ busca_minas/target/debug/deps/busca_minas.d
@@ -0,0 +1,5 @@
+D:\iansh\uba\taller_programacion\busca_minas\target\debug\deps\busca_minas.exe: src\main.rs
+
+D:\iansh\uba\taller_programacion\busca_minas\target\debug\deps\busca_minas.d: src\main.rs
+
+src\main.rs:
